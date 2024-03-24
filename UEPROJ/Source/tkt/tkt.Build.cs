62e1aecc679f927b6f1771631628b7d372999957
nonotor67 <noah.contal@epita.fr>
2024-03-23 23:24:08 -0400

    add repo

---
+++ UEPROJ/Source/tkt/tkt.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class tkt : ModuleRules
+{
+	public tkt(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
